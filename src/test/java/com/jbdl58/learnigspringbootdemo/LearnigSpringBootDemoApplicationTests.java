cf7469ef3d77fa65e1e285b7a414c393db17bedf
Gajendra Raj Sharma <gajendrasharma421@gmail.com>
2023-10-07 14:14:55 +0530

    change tomcat to undertow server

---
+++ src/test/java/com/jbdl58/learnigspringbootdemo/LearnigSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbdl58.learnigspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnigSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
